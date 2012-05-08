68331aafc9683d9905b6d51ad05f351c2888ba8b
Julian Ridden <julian@moodleman.net>
2012-05-08 21:36:08 +1000

    bug fixes

--- layout/general.php
+++ layout/general.php
@@ -1,11 +1,12 @@
 <?php$hasheading = ($PAGE->heading);$hasnavbar = (empty($PAGE->layout_options['nonavbar']) && $PAGE->has_navbar());$hasfooter = (empty($PAGE->layout_options['nofooter']));$hassidepre = $PAGE->blocks->region_has_content('side-pre', $OUTPUT);$hassidepost = $PAGE->blocks->region_has_content('side-post', $OUTPUT);$showsidepre = $hassidepre && !$PAGE->blocks->region_completely_docked('side-pre', $OUTPUT);$showsidepost = $hassidepost && !$PAGE->blocks->region_completely_docked('side-post', $OUTPUT);$custommenu = $OUTPUT->custom_menu();$hascustommenu = (empty($PAGE->layout_options['nocustommenu']) && !empty($custommenu));$haslogo = (!empty($PAGE->theme->settings->logo));$bodyclasses = array();if ($showsidepre && !$showsidepost) {    $bodyclasses[] = 'side-pre-only';} else if ($showsidepost && !$showsidepre) {    $bodyclasses[] = 'side-post-only';} else if (!$showsidepost && !$showsidepre) {    $bodyclasses[] = 'content-only';}if ($hascustommenu) {    $bodyclasses[] = 'has_custom_menu';}echo $OUTPUT->doctype() ?><html <?php echo $OUTPUT->htmlattributes() ?>><head>    <title><?php echo $PAGE->title ?></title>    <link rel="shortcut icon" href="<?php echo $OUTPUT->pix_url('favicon', 'theme')?>" />    <?php echo $OUTPUT->standard_head_html() ?></head><body id="<?php p($PAGE->bodyid) ?>" class="<?php p($PAGE->bodyclasses.' '.join(' ', $bodyclasses)) ?>"><?php echo $OUTPUT->standard_top_of_body_html() ?><div id="page">		<div id="headerwrap">	
 	<div class="headerTable">
-	<?php  echo $OUTPUT->login_info(); ?>
+	
 		<div class="header_left">
 			<div id="logowrap">
 				<a href="/index.php"><div id="logo"></div></a>
 			</div>		</div>
 		<div class="header_right">
+<?php  echo $OUTPUT->login_info(); ?>
 			<div id="menuwrap">				<?php 				if ($hascustommenu) { ?> 					<div id="menuitemswrap"><div id="custommenu"><?php echo $custommenu; ?></div></div>				<?php } ?>			</div>
 		</div>
 	</div>
