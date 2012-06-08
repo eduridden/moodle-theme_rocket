ee87d52dad68dea743d63383964685074e89bfa2
Julian Ridden <julian@moodleman.net>
2012-06-09 00:17:31 +1000

    new renderers for new theme functions

--- renderers.php
+++ renderers.php
@@ -2,6 +2,194 @@
 
 class theme_rocket_core_renderer extends core_renderer {
 
+    /**
+     * Renders a custom menu object (located in outputcomponents.php)
+     *
+     * The custom menu this method override the render_custom_menu function
+     * in outputrenderers.php
+     * @staticvar int $menucount
+     * @param custom_menu $menu
+     * @return string
+     */
+    protected function render_custom_menu(custom_menu $menu) {
+
+		// Generate custom My Courses dropdown
+        $mycourses = $this->page->navigation->get('mycourses');
+
+		if (isloggedin() && $mycourses && $mycourses->has_children()) {
+            $branchlabel = get_string('mycourses', 'theme_rocket');
+            $branchurl   = new moodle_url('/my/index.php');
+            $branchtitle = $branchlabel;
+            $branchsort  = 10000;
+ 
+            $branch = $menu->add($branchlabel, $branchurl, $branchtitle, $branchsort);
+ 
+            foreach ($mycourses->children as $coursenode) {
+                $branch->add($coursenode->get_content(), $coursenode->action, $coursenode->get_title());
+            }
+
+        } else {
+        	$branchlabel = get_string('mycourses', 'theme_rocket');
+            $branchurl   = new moodle_url('/course/index.php');
+            $branchtitle = $branchlabel;
+            $branchsort  = 10000;
+            $branch = $menu->add($branchlabel, $branchurl, $branchtitle, $branchsort);  
+        }
+
+		
+
+		// If the menu has no children return an empty string
+        if (!$menu->has_children()) {
+            return '';
+        }
+
+        // Initialise this custom menu
+        $content = html_writer::start_tag('ul', array('class'=>'dropdown dropdown-horizontal'));
+        // Render each child
+        foreach ($menu->get_children() as $item) {
+            $content .= $this->render_custom_menu_item($item);
+        }
+        // Close the open tags
+        $content .= html_writer::end_tag('ul');
+        // Return the custom menu
+        return $content;
+    }
+
+    /**
+     * Renders a custom menu node as part of a submenu
+     *
+     * The custom menu this method override the render_custom_menu_item function
+     * in outputrenderers.php
+     *
+     * @see render_custom_menu()
+     *
+     * @staticvar int $submenucount
+     * @param custom_menu_item $menunode
+     * @return string
+     */
+    protected function render_custom_menu_item(custom_menu_item $menunode) {
+        // Required to ensure we get unique trackable id's
+        static $submenucount = 0;
+        $content = html_writer::start_tag('li');
+        if ($menunode->has_children()) {
+            // If the child has menus render it as a sub menu
+            $submenucount++;
+            if ($menunode->get_url() !== null) {
+                $url = $menunode->get_url();
+            } else {
+                $url = '#cm_submenu_'.$submenucount;
+            }
+            $content .= html_writer::start_tag('span', array('class'=>'customitem'));
+            $content .= html_writer::link($url, $menunode->get_text(), array('title'=>$menunode->get_title()));
+            $content .= html_writer::end_tag('span');
+            $content .= html_writer::start_tag('ul');
+            foreach ($menunode->get_children() as $menunode) {
+                $content .= $this->render_custom_menu_item($menunode);
+            }
+            $content .= html_writer::end_tag('ul');
+        } else {
+            // The node doesn't have children so produce a final menuitem
+
+            if ($menunode->get_url() !== null) {
+                $url = $menunode->get_url();
+            } else {
+                $url = '#';
+            }
+            $content .= html_writer::link($url, $menunode->get_text(), array('title'=>$menunode->get_title()));
+        }
+        $content .= html_writer::end_tag('li');
+        // Return the sub menu
+        return $content;
+    }
+
+    /**
+     * Copied from core_renderer with one minor change - changed $this->output->render() call to $this->render()
+     *
+     * @param navigation_node $item
+     * @return string
+     */
+    protected function render_navigation_node(navigation_node $item) {
+        $content = $item->get_content();
+        $title = $item->get_title();
+        if ($item->icon instanceof renderable && !$item->hideicon) {
+            $icon = $this->render($item->icon);
+            $content = $icon.$content; // use CSS for spacing of icons
+        }
+        if ($item->helpbutton !== null) {
+            $content = trim($item->helpbutton).html_writer::tag('span', $content, array('class'=>'clearhelpbutton'));
+        }
+        if ($content === '') {
+            return '';
+        }
+        if ($item->action instanceof action_link) {
+            //adds class dimmed to hidden courses and categories
+            $link = $item->action;
+            if ($item->hidden) {
+                $link->add_class('dimmed');
+            }
+            $content = $this->render($link);
+        } else if ($item->action instanceof moodle_url) {
+            $attributes = array();
+            if ($title !== '') {
+                $attributes['title'] = $title;
+            }
+            if ($item->hidden) {
+                $attributes['class'] = 'dimmed_text';
+            }
+            $content = html_writer::link($item->action, $content, $attributes);
+
+        } else if (is_string($item->action) || empty($item->action)) {
+            $attributes = array();
+            if ($title !== '') {
+                $attributes['title'] = $title;
+            }
+            if ($item->hidden) {
+                $attributes['class'] = 'dimmed_text';
+            }
+            $content = html_writer::tag('span', $content, $attributes);
+        }
+        return $content;
+    }
+
+	/**
+     * Reformat edit button to new status indicator
+     */
+
+	public function edit_button(moodle_url $url) {
+		
+		$edittoggle = 'enable';
+        if (!empty($this->page->theme->settings->edittoggle)) {
+            $edittoggle = $this->page->theme->settings->edittoggle;
+        }
+        if ($edittoggle == 'enable') {
+			$editmode = $PAGE->theme->settings->edittoggle;
+			$url->param('sesskey', sesskey());
+			$formclose .='</span><div id="editmode">'.get_string('editmode', 'theme_rocket').'<div id="edittoggle">'.get_string('edittoggle', 'theme_rocket').'&nbsp;</div></div>';
+       	 	if ($this->page->user_is_editing()) {
+				$formopen .= '<span id="on">';
+				$url->param('edit', 'off');
+				$editstring = get_string('turneditingoff','theme_rocket');
+        	} else {
+            	$formopen .='<span id="off">';
+				$url->param('edit', 'on');
+            	$editstring = get_string('turneditingon','theme_rocket');
+        	}
+		return $formopen . $this->single_button($url, $editstring) . $formclose;
+
+		} else {
+         	$url->param('sesskey', sesskey());
+        if ($this->page->user_is_editing()) {
+            $url->param('edit', 'off');
+            $editstring = get_string('turneditingoff');
+        } else {
+            $url->param('edit', 'on');
+            $editstring = get_string('turneditingon');
+        }
+
+        return $this->single_button($url, $editstring);   
+        }
+    }
+
 	/**
      * Outputs the page's footer
      * @return string HTML fragment
@@ -86,53 +274,5 @@ class theme_rocket_core_renderer extends core_renderer {
         return $output;
     }
 
-	public function edit_button(moodle_url $url) {
 
-		$url->param('sesskey', sesskey());
-		$formclose .='</span><div id="editmode">'.get_string('editmode', 'theme_rocket').'<div id="edittoggle">'.get_string('edittoggle', 'theme_rocket').'&nbsp;</div></div>';
-        if ($this->page->user_is_editing()) {
-            $formopen .='<span id="on">';
-			$url->param('edit', 'off');
-            $editstring = get_string('turneditingoff','theme_rocket');
-        } else {
-            $formopen .='<span id="off">';
-			$url->param('edit', 'on');
-            $editstring = get_string('turneditingon','theme_rocket');
-        }
-		return $formopen . $this->single_button($url, $editstring) . $formclose;
-    }
-
-	protected function render_custom_menu(custom_menu $menu) {
- 
-        $mycourses = $this->page->navigation->get('mycourses');
- 
-        if (isloggedin() && $mycourses && $mycourses->has_children()) {
-            $branchlabel = get_string('mycourses', 'theme_rocket');
-            $branchurl   = new moodle_url('/my/index.php');
-            $branchtitle = $branchlabel;
-            $branchsort  = 10000;
- 
-            $branch = $menu->add($branchlabel, $branchurl, $branchtitle, $branchsort);
- 
-            foreach ($mycourses->children as $coursenode) {
-                $branch->add($coursenode->get_content(), $coursenode->action, $coursenode->get_title());
-            }
-        } else {
-        	$branchlabel = get_string('mycourses', 'theme_rocket');
-            $branchurl   = new moodle_url('/course/index.php');
-            $branchtitle = $branchlabel;
-            $branchsort  = 10000;
-            $branch = $menu->add($branchlabel, $branchurl, $branchtitle, $branchsort);
- 
-           
-        }
- 
-        return parent::render_custom_menu($menu);
-	}
- 
-    protected function render_custom_menu_item(custom_menu_item $menunode) {
-        $transmutedmenunode = new transmuted_custom_menu_item($menunode);
-        return parent::render_custom_menu_item($transmutedmenunode);
-    }       
 }
-?>
