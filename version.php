2939340c1794c916bcda683137ff58a5fc87266b
Julian Ridden <julian@moodleman.net>
2012-07-04 18:58:48 +1000

    updated rocket requirements to need moodle 2.3 for this version

--- version.php
+++ version.php
@@ -26,5 +26,5 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2012060800; // The current module version (Date: YYYYMMDDXX)
-$plugin->requires  = 2011081700; // Requires this Moodle version
+$plugin->requires  = 2012062500; // Requires this Moodle version
 $plugin->component = 'theme_rocket'; // Full name of the plugin (used for diagnostics)
