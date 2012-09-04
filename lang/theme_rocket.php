b4ec5a26ef9d6fd937433b9520f060422a6ac5e2
Julian Ridden <julian@moodleman.net>
2012-09-04 15:06:17 +1000

    Adding Greek lang pack

---
+++ lang/theme_rocket.php
@@ -0,0 +1,116 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_rocket', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   moodlecore
+ * @copyright 2011 Julian Ridden
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ $string['pluginname'] = 'Rocket';$string['region-side-post'] = 'Δεξιά';$string['region-side-pre'] = 'Αριστερά';
+$string['region-search'] = 'Αναζήτηση/Προφίλ';$string['region-homeblock'] = 'Banner Αρχικής σελίδας';
+$string['region-toplinks'] = 'Top Links';$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot"><h1>Rocket</h1><img src="rocket/pix/screenshot.jpg" /><h2>About</h2><p>Rocket is a three column fixed-width theme for Moodle 2.1+ originally designed by Julian Ridden to take Moodle themes to a more website focused look and feel. While the design is original, much of the CSS and the icons have come from <a href="http://moodle.org/user/profile.php?id=299683"><strong>Saun Daubney\'s</strong></a> fantastic Aardvark theme and also with support in the forums from <a href="http://moodle.org/user/profile.php?id=713800"><strong>Mary Evans</strong></a>.</p>
+
+<p>The theme is highly customisable through Moodle\'s theme settings. You can adjust images, text and colurs all without changing code. The theme also has a function that means that when users log in, courses they are enrolled in will show up in the dropdown \'custom menu\' at the top of the screen.</p>
+
+<p>Please note that this is listed as a Beta version. There will probably be some bugs here and there. It has been proudly built on a Mac and as such is untested in IE.</p><h1>Instructions</h1>
+<h2>Block Regions</h2>
+<p>This theme is the first, as far as I am aware, to use blocks outside of the standard left and right column methodology. There are two addional locations called \'Search/profile\' and \'Top Link\'s.</p>
+<p>The Top Links region is used on the top right of the homepage and can be used for whatever you design. I find HTML blocks with links or spotlighted info works well.</p>
+<p>The Search/Profile region is specifically designed to have either the \'Global Search\' or the "Logged in user" block stored in it. This region is available on all pages<h2>Logo and Homepage Banner Images</b>
+<h2>Color Schemes</h2>
+<p>In the theme settings screen for Rocket you will find you are able to adjust the color of this theme to suit your branding. Both background colors and menu colors can be adjusted as well as the border colors.</p>
+
+<h2 Additional Footer Text</h2>
+<p>In the theme settings you can also adjust three additonal text fields. The \'Institution Name\' is hsown only on the homepage. This was added as often the name of thei organisation is different to the name of the Moodle site itself.</p>
+<p>';$string['configtitle'] = 'rocket theme';
+
+$string['homeblock'] = 'banner';$string['logo'] = 'Logo';$string['logodesc'] = 'Άλλαξε το logo του θέματος εισάγοντας  το URL μιας νέας εικόνας (π.χ. http://www.mysite.gr/logo.png) Λάβετε υπόψη ότι το αρχικο logo είναι 250X130 px. Ένα διαφανές .png είναι καλύτερο.';$string['footnote'] = 'Footnote';$string['footnotedesc'] = 'Το περιεχόμενο αυτής της περιοχής θα εμφανισθεί στο footer όλων των σελίδων.';
+
+$string['autohide'] = 'Autohide Functionality';$string['autohidedesc'] = 'Αν επιλέξετε αυτή τη λειτουργία θα αλλάξετε την εμφάνιση του Moodle όταν δουλεύετε με ενεργοποιημένη την επεξεργασία. Τα εικονίδια της επεξεργασίας εμφανίζονται μόνο όταν περάσετε τον δείκτη πάνω από κάποιο block. Αυτό κάνει πιο καθαρή την επιφάνεια εργασίας σας.';
+
+$string['bootstrap'] = 'Bootstrap Stying - BETA';
+$string['bootstrapdesc'] = 'This feature will enable new Bootstrap styling. This project for moodle is headed by <a href="http://moodle.org/user/view.php?id=3152&course=5">David Scotson</a> and introduced into Rocket as a Beta status. Bootstrap is a project from Twitter that provides "Simple and flexible HTML, CSS, and Javascript for popular user interface components and interactions." For more inofmration on Bootstrap <a href="http://twitter.github.com/bootstrap/">click here</a>.';
+
+$string['editmodetoggle'] = 'Edit Mode Toggle';$string['edittoggledesc'] = 'Αν το ενεργοποιήσετε αντικαθιστά το "Ενεργοποίηση Επεξεργασίας" με ένα κούμπι.';
+
+$string['enable'] = 'Ενεργοποίηση';
+$string['disable'] = 'Απενεργοποίηση';
+
+$string['mycoursetitle'] = 'Dropdown menu "My Course" Terminology';
+$string['mycoursetitledesc'] = 'Άλλαξε την ονομασία για τον σύνδεσμο "Τα μαθήματά μου" στο αναδιπλούμενο μενού στο επάνω μέρος τησ σελίδας.';
+$string['mycourses'] = 'Τα μαθήματά μου';
+$string['myunits'] = 'My Units';
+$string['mymodules'] = 'My Modules';
+$string['myclasses'] = 'Οι τάξεις μου';
+$string['allcourses'] = 'Όλα τα μαθήματα';
+$string['allunits'] = 'All Units';
+$string['allmodules'] = 'All Modules';
+$string['allclasses'] = 'Όλες οι τάξεις';
+
+$string['copyright'] = 'Copyright Notice';$string['copyrightdesc'] = 'Εδώ μπορείτε να βάλετε σημείωση για πνευματικά δικαιώματα, αν υπάρχουν.';
+
+$string['sitename'] = 'Institution Name';
+$string['sitenamedesc'] = 'Το όνομα του ιδρύματος σας (σχολείο,πανεπιστήμιο κ.α.) προβάλλεται στην αρχική σελίδα. Αυτό δεν είναι το όνομα του ιστοχώρου σας.';
+
+$string['banner'] = 'Banner';
+$string['bannerdesc'] = 'Άλλαξε το banner στην αρχική σελίδα εισάγοντας το URL ενός νέου. (π.χ. http://www.mysite.gr/banner.png). Λάβετε υπόψη σας ότι το αρχικό banner είναι 750X250 px.';
+
+$string['bannerheight'] = 'Ύψος Banner αρχικής σελίδας';
+$string['nobanner'] = 'Απενεργοποίηση Banner';
+$string['bannerheightdesc'] = 'Αν χρησιμοποιείτε Banner μπορείτε εδώ να αλλάξετε το προκαθορισμένο ύψος (250px).';
+
+$string['themecolor'] = 'Χρώμα θέματος';
+$string['themecolordesc'] = 'Τι χρώμα θέλετε για το θέμα σας; Εδώ μπορείτε να ορίσετε το χρώμα που επιθυμείτε.';
+
+$string['themetrimcolor'] = 'Theme Trim Color';
+$string['themetrimcolordesc'] = 'What color should your theme be. This will change mulitple components to produce the color you wish across the moodle site';
+
+$string['menucolor'] = 'Χρώμα Μενού';
+$string['menucolordesc'] = 'Άλλαξε το χρώμα των μενού';
+
+$string['contentlinkcolor'] = 'Χρώμα συνδέσμων περιεχομένου';
+$string['contentlinkcolordesc'] = 'Άλλαξε το χρώμα των συνδέσμων στις περιοχές περιεχομένου';
+
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'Για έμπειρους χρήστες. Μπορείτε εδώ να εισάγετε το δικό σας css που θα αντικαταστήσει το προκαθορισμένο css του θέματος.';
+
+$string['blocklinkcolor'] = 'Block Link Color';
+$string['blocklinkcolordesc'] = 'Άλλαξε το χρώμα των συνδέσμων στα blocks.';
+
+$string['menuhovercolor'] = 'Menu Hover Color';
+$string['menuhovercolordesc'] = 'Άλλαξε το χρώμα των συνδέσμων των μενού, όταν περνάει ο δείκτης από πάνω.';
+
+$string['menutrimcolor'] = 'Menu Trim Color';
+$string['menutrimcolordesc'] = 'Change the color used to trim the edges of the menu';
+
+$string['menulinkcolor'] = 'Menu Link Color';
+$string['menulinkcolordesc'] = 'Άλλαξε το χρώμα των συνδέσμων των μενού.';
+
+$string['navigation'] = 'Πλοήγηση';
+
+$string['editingmode'] = '<strong>Η επεξεργασία είνα ενεργή:</strong> Περάστε τον δείκτη πάνω από ένα block ή ένα αντικείμενο που επιθυμείτε να επεξεργαστείτε.Χρησιμοποιόντας το Drag and drop σύρετε αρχεία στα τμήματα του μαθήματος για να τα αναβάσετε';
+
+$string['screenwidth'] = 'Πλάτος οθόνης';
+$string['screenwidthdesc'] = 'Εδώ μπορείς να ορίσεις αν το πλάτος του θέματος θα είναι ορισμένο στα 1000px ή θα αλλάζει σε 100% (της οθόνης). <br> Σημειώστε ότι η αρχίκη σελίδα θα είναι πάντα σε σταθερό πλάτος.';
+$string['fixedwidth'] = 'Σταθερό πλάτος';
+$string['variablewidth'] = 'Μεταβλήτό πλάτος';
+$string['turneditingoff'] = 'On';
+$string['turneditingon'] = 'Off';
+$string['editmode'] = 'Η επεξεργασία είναι:';
+$string['edittoggle'] = 'Κλικ για αλλαγή';
