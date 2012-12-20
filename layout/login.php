2f4b9664526b8d372a13ee861c0875983861da04
Julian Ridden <julian@moodleman.net>
2012-12-21 10:55:28 +1100

    Adding License info

--- layout/login.php
+++ layout/login.php
@@ -1,4 +1,29 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * General layout for the mymobile theme
+ *
+ * @package    theme
+ * @subpackage rocket
+ * @copyright  Julian Ridden
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// get settings
 
 $hasheading = ($PAGE->heading);
 $hasnavbar = (empty($PAGE->layout_options['nonavbar']) && $PAGE->has_navbar());
@@ -14,6 +39,9 @@ $custommenu = $OUTPUT->custom_menu();
 $hascustommenu = (empty($PAGE->layout_options['nocustommenu']) && !empty($custommenu));
 
 $hasfootnote = (!empty($PAGE->theme->settings->footnote));
+
+// load lang strings
+$editingmode = get_string('editingmode', 'theme_rocket');
 
 $bodyclasses = array();
 if ($showsidepre && !$showsidepost) {
