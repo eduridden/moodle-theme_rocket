1b90ce7f8e61a50e46d16e35444e03068faf59db
Julian Ridden <julian@moodleman.net>
2013-01-15 14:36:50 +1100

    Adding Secure laoyout to config

--- config.php
+++ config.php
@@ -67,6 +67,13 @@ $THEME->layouts = array(
         'regions' => array('side-pre','search'),
         'defaultregion' => 'side-pre',
     ),
+	// The pagelayout used for safebrowser and securewindow.
+    'secure' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post'),
+        'defaultregion' => 'side-pre',
+        'options' => array('nofooter'=>true, 'nonavbar'=>true, 'nocustommenu'=>true, 'nologinlinks'=>true, 'nocourseheaderfooter'=>true),
+    ),
     // My dashboard page
     'mydashboard' => array(
         'file' => 'general.php',
