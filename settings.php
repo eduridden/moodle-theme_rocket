b798e4122104022188600c8de839c957c7ac3111
Julian Ridden <julian@moodleman.net>
2013-07-09 10:42:31 +1000

    Theme cache's will reset after any changes are made to theme

--- settings.php
+++ settings.php
@@ -4,7 +4,8 @@ defined('MOODLE_INTERNAL') || die;
 
 if ($ADMIN->fulltree) {
 
-// Institution Name$name = 'theme_rocket/sitename';$title = get_string('sitename','theme_rocket');$description = get_string('sitenamedesc', 'theme_rocket');$default = 'Welcome to my site';$setting = new admin_setting_configtext($name, $title, $description, $default);$settings->add($setting);
+// Institution Name$name = 'theme_rocket/sitename';$title = get_string('sitename','theme_rocket');$description = get_string('sitenamedesc', 'theme_rocket');$default = 'Welcome to my site';$setting = new admin_setting_configtext($name, $title, $description, $default);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
 // Set status of Autohide functionality
 $name = 'theme_rocket/autohide';
@@ -16,6 +17,7 @@ $choices = array(
 	'disable' => get_string('disable', 'theme_rocket')
 );
 $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+$setting->set_updatedcallback('theme_reset_all_caches');
 $settings->add($setting);
 
 // Set status of Edit Toggle functionality
@@ -28,6 +30,7 @@ $choices = array(
 	'disable' => get_string('disable', 'theme_rocket')
 );
 $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+$setting->set_updatedcallback('theme_reset_all_caches');
 $settings->add($setting);
 
 // Set status of Bootstrap functionality
@@ -40,6 +43,7 @@ $choices = array(
 	'disable' => get_string('disable', 'theme_rocket')
 );
 $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+$setting->set_updatedcallback('theme_reset_all_caches');
 $settings->add($setting);
 
 // Set terminology for dropdown couse list
@@ -54,11 +58,14 @@ $choices = array(
 	'module' => get_string('mymodules', 'theme_rocket')
 );
 $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+$setting->set_updatedcallback('theme_reset_all_caches');
 $settings->add($setting);// Logo file setting$name = 'theme_rocket/logo';$title = get_string('logo','theme_rocket');$description = get_string('logodesc', 'theme_rocket');
-$default = 'rocket/pix/logo/rocket.png';$setting = new admin_setting_configfile($name, $title, $description, $default);$settings->add($setting);
+$default = 'rocket/pix/logo/rocket.png';$setting = new admin_setting_configfile($name, $title, $description, $default);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
 // Banner file setting$name = 'theme_rocket/banner';$title = get_string('banner','theme_rocket');$description = get_string('bannerdesc', 'theme_rocket');
-$default = 'rocket/pix/banner/default.png';$setting = new admin_setting_configtext($name, $title, $description, $default);$settings->add($setting);
+$default = 'rocket/pix/banner/default.png';$setting = new admin_setting_configtext($name, $title, $description, $default);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
 // Banner Height
 $name = 'theme_rocket/bannerheight';
@@ -67,6 +74,7 @@ $description = get_string('bannerheightdesc', 'theme_rocket');
 $default = 255;
 $choices = array(5=>get_string('nobanner', 'theme_rocket'), 55=>'50px', 105=>'100px',155=>'150px', 205=>'200px', 255=>'250px',  305=>'300px',355=>'350px');
 $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+$setting->set_updatedcallback('theme_reset_all_caches');
 $settings->add($setting);
 
 // Fullscreen Toggle
@@ -76,29 +84,41 @@ $description = get_string('screenwidthdesc', 'theme_rocket');
 $default = 1000;
 $choices = array(1000=>get_string('fixedwidth','theme_rocket'), 97=>get_string('variablewidth','theme_rocket'));
 $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);
+$setting->set_updatedcallback('theme_reset_all_caches');
 $settings->add($setting);
-// Main theme background colour setting$name = 'theme_rocket/themecolor';$title = get_string('themecolor','theme_rocket');$description = get_string('themecolordesc', 'theme_rocket');$default = '#a8213a';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);$settings->add($setting);
+// Main theme background colour setting$name = 'theme_rocket/themecolor';$title = get_string('themecolor','theme_rocket');$description = get_string('themecolordesc', 'theme_rocket');$default = '#a8213a';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
-// Main theme trim colour setting$name = 'theme_rocket/themetrimcolor';$title = get_string('themetrimcolor','theme_rocket');$description = get_string('themetrimcolordesc', 'theme_rocket');$default = '#660000';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);$settings->add($setting);
+// Main theme trim colour setting$name = 'theme_rocket/themetrimcolor';$title = get_string('themetrimcolor','theme_rocket');$description = get_string('themetrimcolordesc', 'theme_rocket');$default = '#660000';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
-// Menu colour setting$name = 'theme_rocket/menucolor';$title = get_string('menucolor','theme_rocket');$description = get_string('menucolordesc', 'theme_rocket');$default = '#76777c';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);$settings->add($setting);
+// Menu colour setting$name = 'theme_rocket/menucolor';$title = get_string('menucolor','theme_rocket');$description = get_string('menucolordesc', 'theme_rocket');$default = '#76777c';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
-// Menu hover colour setting$name = 'theme_rocket/menuhovercolor';$title = get_string('menuhovercolor','theme_rocket');$description = get_string('menuhovercolordesc', 'theme_rocket');$default = '#8a8a8a';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);$settings->add($setting);
+// Menu hover colour setting$name = 'theme_rocket/menuhovercolor';$title = get_string('menuhovercolor','theme_rocket');$description = get_string('menuhovercolordesc', 'theme_rocket');$default = '#8a8a8a';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
-// Menu trim colour setting$name = 'theme_rocket/menutrimcolor';$title = get_string('menutrimcolor','theme_rocket');$description = get_string('menutrimcolordesc', 'theme_rocket');$default = '#4c4c4c';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);$settings->add($setting);
+// Menu trim colour setting$name = 'theme_rocket/menutrimcolor';$title = get_string('menutrimcolor','theme_rocket');$description = get_string('menutrimcolordesc', 'theme_rocket');$default = '#4c4c4c';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
-// Content link colour setting$name = 'theme_rocket/contentlinkcolor';$title = get_string('contentlinkcolor','theme_rocket');$description = get_string('contentlinkcolordesc', 'theme_rocket');$default = '#006699';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);$settings->add($setting);
+// Content link colour setting$name = 'theme_rocket/contentlinkcolor';$title = get_string('contentlinkcolor','theme_rocket');$description = get_string('contentlinkcolordesc', 'theme_rocket');$default = '#006699';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
-// Block link colour setting$name = 'theme_rocket/blocklinkcolor';$title = get_string('blocklinkcolor','theme_rocket');$description = get_string('blocklinkcolordesc', 'theme_rocket');$default = '#333333';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);$settings->add($setting);
+// Block link colour setting$name = 'theme_rocket/blocklinkcolor';$title = get_string('blocklinkcolor','theme_rocket');$description = get_string('blocklinkcolordesc', 'theme_rocket');$default = '#333333';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
-// Menu link colour setting$name = 'theme_rocket/menulinkcolor';$title = get_string('menulinkcolor','theme_rocket');$description = get_string('menulinkcolordesc', 'theme_rocket');$default = '#ffffff';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);$settings->add($setting);// Footer text or link$name = 'theme_rocket/footnote';$title = get_string('footnote','theme_rocket');$description = get_string('footnotedesc', 'theme_rocket');$default = '';$setting = new admin_setting_confightmleditor($name, $title, $description, $default);$settings->add($setting);
-// Copyright Notice$name = 'theme_rocket/copyright';$title = get_string('copyright','theme_rocket');$description = get_string('copyrightdesc', 'theme_rocket');$default = '';$setting = new admin_setting_confightmleditor($name, $title, $description, $default);$settings->add($setting);
+// Menu link colour setting$name = 'theme_rocket/menulinkcolor';$title = get_string('menulinkcolor','theme_rocket');$description = get_string('menulinkcolordesc', 'theme_rocket');$default = '#ffffff';$previewconfig = NULL;$setting = new admin_setting_configcolourpicker($name, $title, $description, $default, $previewconfig);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);// Footer text or link$name = 'theme_rocket/footnote';$title = get_string('footnote','theme_rocket');$description = get_string('footnotedesc', 'theme_rocket');$default = '';$setting = new admin_setting_confightmleditor($name, $title, $description, $default);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
+// Copyright Notice$name = 'theme_rocket/copyright';$title = get_string('copyright','theme_rocket');$description = get_string('copyrightdesc', 'theme_rocket');$default = '';$setting = new admin_setting_confightmleditor($name, $title, $description, $default);
+$setting->set_updatedcallback('theme_reset_all_caches');$settings->add($setting);
 
 // Custom CSS file
 $name = 'theme_rocket/customcss';
 $title = get_string('customcss','theme_rocket');
 $description = get_string('customcssdesc', 'theme_rocket');
 $setting = new admin_setting_configtextarea($name, $title, $description, '');
+$setting->set_updatedcallback('theme_reset_all_caches');
 $settings->add($setting);
 
 }
